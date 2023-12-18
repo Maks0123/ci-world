48eeefb211eb949bd772a4b8d966e7651ca7fb35
administrator <vadimcorporateukr@gmail.com>
2023-12-19 01:02:39 +0300

    ci_com 10

--- jenkins/stages/Tests.groovy
+++ jenkins/stages/Tests.groovy
@@ -3,8 +3,6 @@
 def UnitTetst(String project, String stageName='') {
     stage(stageName ? stageName : 'UnitTetst') {
         println('UnnitTetst... ')
-        comm = "sleep 10"
-        sh(comm)
     }
 }
 
