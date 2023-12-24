c25fad7795e3c4176e66b007735164ff66b49a48
administrator <vadimcorporateukr@gmail.com>
2023-12-24 21:08:47 +0300

    ext1

--- jenkins/stages/Helm.groovy
+++ jenkins/stages/Helm.groovy
@@ -6,8 +6,8 @@ def HelmPackage(String project, String version, String stageName='') {
             String comm
             println('Package chart... ')
 
-            comm = """helm package ${project} --version ${version} \\
-                    |curl -X PUT -T "${project}/${project}-${version}" https://lab-bucket-nure.s3.amazonaws.com""".stripMargin()
+            comm = """helm package ${project} --version ${version}
+                    |curl -X PUT -T "${project}/${project}-${version}" https://lab-bucket-nure.s3.amazonaws.com/${project}/""".stripMargin()
             sh(comm)
         }
     }
